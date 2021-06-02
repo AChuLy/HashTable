ec6cfecd0b6a7aaae64227be9a7a2550701d5d9d
AChuLy <79276723+AChuLy@users.noreply.github.com>
2021-06-02 23:01:20 +0700

    Add files via upload

---
+++ HashTableApp/HashTableApp/HashTableApp/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HashTableApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Hashtable hashtable;
+        public MainWindow()
+        {
+            InitializeComponent();
+            hashtable = new Hashtable();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            //add
+            hashtable.Add(key.Text,value.Text);
+            key.Text = "";
+            value.Text = "";
+                      
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //remove key only
+            hashtable.Remove(key.Text);
+            key.Text = "";
+
+        }
+
+        private void ShowAll_Click(object sender, RoutedEventArgs e)
+        {
+            ICollection icollection = hashtable.Keys;
+            //showall
+            foreach(string key in icollection)
+            {
+                MessageBox.Show(hashtable[key].ToString());
+            }
+        }
+    }
+}
